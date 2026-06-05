92bdb9944cf1688792e77eff40d8a229dbd7384b
Manuel3001 <manuel.schwab.ms11@gmail.com>
2026-06-05 18:49:09 +0200

    chores: added an primitiv homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
-import ComparePage from './ComparePage';
-import DiscoveryPage from './DiscoveryPage';
+import ComparePage from './pages/ComparePage';
+import DiscoveryPage from './pages/DiscoveryPage';
+import HomePage from './pages/HomePage';
 export default function App() {
   return (
     <BrowserRouter>
     {/* a simple navigation bar for testing */}
         <nav>
-            <Link to="/">Discovery</Link>
+            <Link to="/discovery">Discovery</Link>
             <Link to="/compare">Compare</Link>
+            <Link to="/">Home</Link>
         </nav>
 
         {/* Define the routes */}
         <Routes>
-            <Route path="/" element={<DiscoveryPage />} />
+            <Route path="/discovery" element={<DiscoveryPage />} />
             <Route path="/compare" element={<ComparePage />} />
+            <Route path="/" element={<HomePage />} />
         </Routes>
     </BrowserRouter>
   );
