bd4c3cda3aea4e6635850f11796112034de42a87
Manuel3001 <manuel.schwab.ms11@gmail.com>
2026-06-05 20:49:09 +0200

    chore: configured the index.html and main.tsx file

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import {createRoot} from "react-dom/client";
+import App from "./App";
+import {BrowserRouter} from "react-router-dom";
+
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+const root = createRoot(rootElement);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
