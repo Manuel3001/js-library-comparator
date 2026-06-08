a9e55c69ffa4d739616b92dde52e7a1c712fc87f
Manuel3001 <manuel.schwab.ms11@gmail.com>
2026-06-08 10:06:50 +0200

    chore: added a global stylesheet index.css

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,9 +1,6 @@
+import React from 'react';
 import styles from './PageLayout.module.css';
 
 export default function PageLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className={styles.pageLayout}>
-      {children}
-    </div>
-  );
+  return <div className={styles.pageWrapper}>{children}</div>;
 }
