3f699e8befb77ba09a3a96ce200788bdba51a763
Manuel3001 <manuel.schwab.ms11@gmail.com>
2026-06-08 10:34:10 +0200

    chore: adding a header on the homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,18 @@
   import PageLayout from '../components/PageLayout';
   import styles from './moduleStyles/HomePage.module.css';
+  import { Link } from 'react-router-dom';
 
 export default function HomePage() {
   return (
     <PageLayout>
-      <h1 className={styles.h1}>Home Page</h1>
-      <p>Welcome to the home page. Use the navigation links to explore the discovery and compare pages.</p>
+      <header className={styles.header}>
+      <h1 className={styles.title}>Javascript Fatigue is ending here.</h1>
+      <p className={styles.subtitle}>Compare the bundle-sizes, security track records, and performance metrics of the most popular JavaScript libraries.</p>
+      <div className={styles.buttonGroup}>
+      <Link to="/" className={styles.primaryButton}>Discover your Library</Link>
+      <Link to="/compare" className={styles.secondaryButton}>Compare Libraries</Link>
+      </div>
+      </header>
     </PageLayout>
   );
 }
